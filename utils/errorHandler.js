43184e97f341c228294221bea5e2d610f34a9a74
rlicata34 <rlicata34@gmail.com>
2024-12-02 15:17:13 -0500

    FIx errorhandler.js

--- utils/errorHandler.js
+++ utils/errorHandler.js
@@ -5,9 +5,10 @@ const handleError = (err, res) => {
 
   if (err.name === 'ValidationError' || err.name === 'CastError') {
     return res.status(ERROR_CODES.BAD_REQUEST).json({ message: err.message });
-    } else if (err.name === 'DocumentNotFoundError') {
-        return res.status(ERROR_CODES.NOT_FOUND).json({ message: err.message });
-      }
+    } 
+  if (err.name === 'DocumentNotFoundError') {
+      return res.status(ERROR_CODES.NOT_FOUND).json({ message: err.message });
+    }
 
   return res.status(ERROR_CODES.SERVER_ERROR).json({ message: err.message });
 };
