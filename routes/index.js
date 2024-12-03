abcfe5766a538142b0b230d829710bb783697997
rlicata34 <rlicata34@gmail.com>
2024-12-03 12:00:54 -0500

    Constant for status code in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const router = require("express").Router();
 const userRouter = require("./users");
 const clothingItemRouter = require("./clothingItems");
-const handleError = require("../utils/errorHandler");
+const { BAD_REQUEST } = require("../utils/errors");
 
 router.use("/users", userRouter);
 router.use("/items", clothingItemRouter);
 
-router.use(handleError());
+router.use((req, res) => {
+  res.status(BAD_REQUEST).send({ message: "Requested resource not found" });
+});
 
 module.exports = router;
