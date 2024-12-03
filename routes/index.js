6ef4392ebd4257f5af72cda685491cc4f26126fa
rlicata34 <rlicata34@gmail.com>
2024-12-03 12:03:28 -0500

    Replace bad request with not found in index.js

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 const router = require("express").Router();
 const userRouter = require("./users");
 const clothingItemRouter = require("./clothingItems");
-const { BAD_REQUEST } = require("../utils/errors");
+const { NOT_FOUND } = require("../utils/errors");
 
 router.use("/users", userRouter);
 router.use("/items", clothingItemRouter);
 
 router.use((req, res) => {
-  res.status(BAD_REQUEST).send({ message: "Requested resource not found" });
+  res.status(NOT_FOUND).send({ message: "Requested resource not found" });
 });
 
 module.exports = router;
