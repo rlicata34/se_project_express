30a0a0c4677b67b26f041c8396083581c06ce477
rlicata34 <rlicata34@gmail.com>
2024-12-04 17:31:50 -0500

    Update create user controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 const handleError = require('../utils/errorHandler');
 
@@ -8,11 +9,42 @@ const getUsers = (req, res) => {
 };
 
 const createUser = (req, res) => {
-    const { name, avatar } = req.body;
+    const { name, avatar, email, password } = req.body;
     
-    User.create({ name, avatar })
-        .then((user) => res.status(201).send(user))
-        .catch((err) => handleError(err, res));
+    // Check if email already exists
+    User.findOne({ email })
+        .then((existingUser) => {
+            if (existingUser) {
+                // Throw an error if email exists
+                const error = new Error("Email is already in use.");
+                error.statusCode = 409; // HTTP 409 Conflict
+                throw error;
+            }
+            // Hash the password
+            return bcrypt.hash(password, 10);
+        })
+        .then((hash) =>
+            // Create the user in the database
+            User.create({ name, avatar, email, password: hash })
+        )
+        .then((user) =>
+        // Send the created user as a response
+        res.status(201).send({
+            id: user._id,
+            name: user.name,
+            email: user.email,
+            avatar: user.avatar,
+        })
+        )
+        .catch((err) => {
+            // Handle MongoDB duplicate key error
+            if (err.code === 11000) {
+                err.message = "Email already exists.";
+                err.statusCode = 409;
+            }
+            // Use custom error handler
+            handleError(err, res);
+        });
 };
 
 const getUser = (req, res) => {
