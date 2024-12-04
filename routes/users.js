0a944da7894911f86ab10827b0ff9a17492d6d7d
rlicata34 <rlicata34@gmail.com>
2024-12-04 17:54:00 -0500

    Add routes and controllers for signing up and signing in

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
 const router = require("express").Router()
-const { getUsers, createUser, getUser } = require("../controllers/users");
+const { createUser, login } = require("../controllers/users");
 
-router.get("/", getUsers);
-router.get("/:userId", getUser);
-router.post("/", createUser);
+router.post("/signup", createUser);
+router.post("/signin", login);
 
 module.exports = router;
 
