591154a4af336068b7b2a30a6149fb485e79447a
rlicata34 <rlicata34@gmail.com>
2025-01-16 17:19:34 -0500

    Properly import validateUpdateUserBody in users.js

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const router = require("express").Router()
 const { auth } = require("../middlewares/auth");
-const validateUpdateUserBody = require("../middlewares/validation");
+const { validateUpdateUserBody } = require("../middlewares/validation");
 const { getCurrentUser, updateProfile } = require("../controllers/users");
 
 router.get("/me", auth, getCurrentUser);
